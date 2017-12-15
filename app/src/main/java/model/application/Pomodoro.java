a280dfcadab070826b990ea7b020a929e58d831a
Diego <diegoal_2098@hotmail.com>
2017-12-15 12:53:23 -0500

    Implementing pomodoro with Timer and TimerTask

--- app/src/main/java/model/application/Pomodoro.java
+++ app/src/main/java/model/application/Pomodoro.java
@@ -1,18 +1,66 @@
 package model.application;
 
-import io.github.krtkush.lineartimer.LinearTimer;
-import io.github.krtkush.lineartimer.LinearTimerCountDownTimer;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * @author diego on 14/12/2017.
+ * Implementation based on: https://stackoverflow.com/questions/14393423/how-to-make-a-countdown-timer-in-java
  */
 
-public class Pomodoro {
-    public Pomodoro( long millisLeftPomodoro, long millisLeftBreak ){
-//        LinearTimerCountDownTimer timer = new LinearTimerCountDownTimer(millisLeftPomodoro,1, );
+public class Pomodoro extends Thread {
+
+    /**
+     * Timer
+     */
+    private Timer timer;
+
+    /**
+     * Duration of timer
+     */
+    private int interval;
+
+    /**
+     * Creates a new pomodoro that lasts pInterval seconds
+     * @param pInterval Duration of pomodoro in seconds
+     */
+    public Pomodoro( int pInterval ){
+        interval = pInterval;
     }
 
-    public void start(){
+    /**
+     * Creates a new timer and runs it
+     */
+    @Override
+    public void run(){
+        int delay = 1000;
+        int period = 1000;
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new Task(), delay, period );
+    }
 
+    /**
+     * Decreases the time left by a second and stops the timer when the time is up.
+     * @return Time left in the timer
+     */
+    public int setInterval(){
+        if( interval == 1 )
+            timer.cancel();
+        return --interval;
+    }
+
+    /**
+     * Task class for the timer
+     */
+    private class Task extends TimerTask{
+
+        /**
+         * Calls the setInterval method, which decreases the time interval.
+         * It also prints the time left.
+         */
+        @Override
+        public void run() {
+            System.out.println( setInterval());
+        }
     }
 }
