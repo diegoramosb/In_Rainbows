14cf20a3e443a72e9106ff2b44420533be32dcfb
Unknown <diegoal_2098@hotmail.com>
2018-01-16 22:19:16 -0500

    Tests fixed

--- app/src/test/java/com/example/diego/inrainbows/application/SemesterTest.java
+++ app/src/test/java/com/example/diego/inrainbows/application/SemesterTest.java
@@ -23,6 +23,7 @@ public class SemesterTest {
         semester = new Semester(2018, 1, 22, 2018, 5, 12);
     }
 
+    @Before
     public void setUp2(){
         setUp();
 
@@ -37,6 +38,26 @@ public class SemesterTest {
 
 
         Subject subject2 = new Subject("a", 1, 1);
+        Task task21 = new Task("a", "", 20, 4, true, true, true);
+        subject2.addTask(task21);
+        semester.addSubject(subject2);
+    }
+
+    @Before
+    public void setUp3(){
+        setUp();
+
+        Subject subject1 = new Subject("a", 3, 4);
+        Task task11 = new Task("a", "", 20, 5.0, true, true, true);
+        Task task12 = new Task("b", "", 20, 5.0, true, true, true);
+        Task task13 = new Task("c", "", 60, 5.0, true, true, true);
+        subject1.addTask(task11);
+        subject1.addTask(task12);
+        subject1.addTask(task13);
+        semester.addSubject(subject1);
+
+
+        Subject subject2 = new Subject("b", 1, 1);
         Task task21 = new Task("a", "", 20, 4, true, true, true);
         subject2.addTask(task21);
         semester.addSubject(subject2);
@@ -63,7 +84,7 @@ public class SemesterTest {
             fail();
         }catch (IllegalArgumentException e){}
         try{
-            semester.setStartDate(2018, 06, 20);
+            semester.setStartDate(2018, 6, 20);
             fail();
         }catch (AssertionError e){}
         try{
@@ -84,11 +105,11 @@ public class SemesterTest {
             fail();
         }catch (IllegalArgumentException e){}
         try{
-            semester.setEndDate(2018, 01, 05);
+            semester.setEndDate(2018, 1, 5);
             fail();
         }catch (AssertionError e){}
         try{
-            semester.setEndDate(2018, 06, 22);
+            semester.setEndDate(2018, 6, 22);
             assertEquals(2018, semester.getEndDate().getYear());
             assertEquals(6, semester.getEndDate().getMonthOfYear());
             assertEquals(22, semester.getEndDate().getDayOfMonth());
@@ -105,18 +126,15 @@ public class SemesterTest {
 
     @Test
     public void getCurrentWeek() {
-        //TODO Finish dates stuff
+        //TODO Find a way to get start and end instants of a period.
         try {
             setUp();
-            System.out.println(semester.getCurrentWeek());
+//            System.out.println(semester.getCurrentWeek());
         }catch (Exception e){
             e.printStackTrace();
         }
     }
 
-    @Test
-    public void getWeeks()  {
-    }
 
     @Test
     public void getCurrentWeekNumber() {
@@ -131,13 +149,13 @@ public class SemesterTest {
 
     @Test
     public void getCurrentGPA() {
-        setUp2();
+        setUp3();
         assertEquals(4.75, semester.getCurrentGPA(), 0.001);
     }
 
     @Test
     public void getCredits() {
-        setUp2();
+        setUp3();
         assertEquals(4, semester.getCredits(), 0.0);
     }
 
