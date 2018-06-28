47f6e9dd6f1e50d911cf0856ace7dc3a97e25661
Diego Ramos <diegoal_2098@hotmail.com>
2018-06-28 16:22:38 -0500

    Cambios en Gradle e importando javax

---
+++ app/src/main/java/presenter/SemesterPresenter.java
@@ -0,0 +1,7 @@
+package presenter;
+
+/**
+ * @author diego on 28/06/2018.
+ */
+public class SemesterPresenter {
+}
---
+++ app/src/main/java/view/SemesterView.java
@@ -0,0 +1,7 @@
+package view;
+
+/**
+ * @author diego on 28/06/2018.
+ */
+public interface SemesterView extends View {
+}
